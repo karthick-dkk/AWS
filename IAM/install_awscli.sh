a49df973f0663ce24bff9323f063cab56db50242
karthick-dkk <128692563+karthick-dkk@users.noreply.github.com>
2024-01-05 09:49:40 +0530

    Update install_awscli.sh

--- IAM/install_awscli.sh
+++ IAM/install_awscli.sh
@@ -9,5 +9,9 @@ echo "Unziping "
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
 sudo ln -s /usr/local/aws-cli/v2/2.15.7/bin/aws /bin/aws
 aws 
-
 if [ $? -eq 0 ]; then echo -e "\nInsatllation ok"; else echo -e "\nHey i thing AWS not installed properly, Please try again "; fi
+#################
+# Install jq
+#sudo yum install jq -y   #Redhat,Centos,Rocky 
+#sudo apt install jq -y   # ubuntu
+################
