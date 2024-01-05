842228ce141be0cd7219f159dfc553e7d14a835b
karthick-dkk <128692563+karthick-dkk@users.noreply.github.com>
2024-01-05 06:47:07 +0530

    Update install_awscli.sh

--- IAM/install_awscli.sh
+++ IAM/install_awscli.sh
@@ -7,7 +7,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 echo "Unziping "
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
-
+sudo ln -s /usr/local/aws-cli/v2/2.15.7/bin/aws /bin/aws
 aws 
 
 if [ $? -eq 0 ]; then echo -e "\nInsatllation ok"; else echo -e "\nHey i thing AWS not installed properly, Please try again "; fi
