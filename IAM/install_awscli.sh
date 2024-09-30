3417c62986a18a724a23fb703c131c46f8a3d5cc
Karthick D <128692563+karthick-dkk@users.noreply.github.com>
2024-09-30 11:22:05 +0530

    Update install_awscli.sh

--- IAM/install_awscli.sh
+++ IAM/install_awscli.sh
@@ -1,12 +1,11 @@
 #########################
 # Install AWS CLI linux
 ########################
-
+sudo apt install unzip -y
 echo "Download AWS CLIv2"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-echo "Unziping "
+echo "Unziping"
 sudo ./aws/install
-aws 
+aws --version
 if [ $? -eq 0 ]; then echo -e "\nInsatllation ok"; else echo -e "\nHey i thing AWS not installed properly, Please try again "; fi
-#################
